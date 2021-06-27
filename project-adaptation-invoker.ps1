a3d019f94eb39d976f6c32df318943293a6b2337
Ricky8955555 <397050061@qq.com>
2021-06-27 14:57:10 +0800

    Test

--- project-adaptation-invoker.ps1
+++ project-adaptation-invoker.ps1
@@ -3,7 +3,6 @@ $old_proj_path = "$proj_dir/Nethard Music-vs2008.csproj"
 $new_proj_path = "$proj_dir/Nethard Music.csproj"
 
 $script_path = "project-adaptation.ps1"
-Write-Host (git diff-tree --no-commit-id --name-only -r HEAD)
 
 $diff = git diff-tree --no-commit-id --name-only -r HEAD
 
