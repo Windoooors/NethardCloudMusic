5c8a4538f3fff27b1f83fbd3fc62f0ee3c69e5f3
Ricky8955555 <397050061@qq.com>
2021-06-27 20:46:52 +0800

    fix adaptation script.

--- project-adaptation.ps1
+++ project-adaptation.ps1
@@ -1,7 +1,7 @@
 param([int]$action)
 $ErrorActionPreference = 'Stop'
 
-$proj_dir = "Nethard Music"
+$proj_dir = "C:\Users\mokin\source\repos\nethard-cloud-music\Nethard Music"
 $old_proj_path = "$proj_dir\Nethard Music-vs2008.csproj"
 $new_proj_path = "$proj_dir\Nethard Music.csproj"
 
@@ -9,12 +9,14 @@ if ($action -eq 0)
 {
     $xml = [xml](Get-Content $old_proj_path)
     $xml.Project.SetAttribute("ToolsVersion", "Current")
+    $xml.Project.PropertyGroup[0].LangVersion = "3.0"
     $xml.Save($new_proj_path)
 }
 elseif ($action -eq 1)
 {
     $xml = [xml](Get-Content $new_proj_path)
     $xml.Project.SetAttribute("ToolsVersion", "3.5")
+    $xml.Project.PropertyGroup[0].LangVersion = "Default"
     $xml.Save($old_proj_path)
 }
 else
