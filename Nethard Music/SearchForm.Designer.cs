08831df5f9998b680607e97df156017ea0642def
Setchin <2794813909@qq.com>
2021-11-21 10:25:07 +0800

    为搜索窗体增加 AcceptButton

--- Nethard Music/SearchForm.Designer.cs
+++ Nethard Music/SearchForm.Designer.cs
@@ -90,6 +90,7 @@ namespace Setchin.NethardMusic
             // 
             // SearchForm
             // 
+            this.AcceptButton = this.searchButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(294, 340);
